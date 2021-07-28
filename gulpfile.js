97aeb24e9cf843e870f39828f78a2b5dbaa38bb7
YoimelDev <yoimeldev@gmail.com>
2021-07-28 12:35:21 -0400

    minified images

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 // Funcion que compila SASS
 function css() {
@@ -18,10 +19,17 @@ function minificarcss() {
         .pipe( dest('./build/css') )
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+    .pipe( imagemin() )
+    .pipe( dest('./build/img') )
+}
+
 function watchArchivos() {
     watch('src/scss/**/*.scss', css); // * = carpeta actual - ** = todos los archivos con esa extension
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
