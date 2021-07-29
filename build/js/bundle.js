8aa80d2d1cf79d55cd4d7f25cc0c98dea68cd790
YoimelDev <yoimeldev@gmail.com>
2021-07-28 21:59:34 -0400

    function mostrarImagen added

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -9,10 +9,29 @@ function crearGaleria() {
     for(let i = 1; i <= 12; i++) {
         const imagen = document.createElement('IMG');
         imagen.src = `build/img/thumb/${i}.webp`;
+        imagen.dataset.imagenId = i;
+
+        // Anadir la funcion de mostrarImagen
+        imagen.onclick = mostrarImagen;
         
         const lista = document.createElement('LI');
         lista.appendChild(imagen);
 
         galeria.appendChild(lista);
     }
+}
+
+function mostrarImagen(e) {
+    const id = parseInt(e.target.dataset.imagenId);
+
+    // Generar la imagen
+    const imagen = document.createElement('IMG');
+    imagen.src = `build/img/grande/${id}.webp`;
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+
+    // Mostrar en el html
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
 }
